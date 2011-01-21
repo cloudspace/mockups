237d3e23db0b3dffde6e901d42841e65b4e7d409
A Flores <ctrl4ltdeleteme@gmail.com>
2011-01-21 15:10:19 -0500

    [9046399] for Add unit testing created a client stub

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,7 @@
 var sys = require('sys')
 var assert = require('assert')
 
+
 exports.it = function(description, test) {
   try {
     test();
@@ -11,3 +12,20 @@ exports.it = function(description, test) {
 		sys.puts("\n");
   }
 }
+
+var Client = function (data) {
+		if(!data){
+			data = {remoteAddress: '127.0.0.1', sessionId: rand(10000) }
+		}
+		this.connection = {remoteAddress: data.remoteAddress};
+		this.sessionId = data.sessionId;
+};
+	
+Client.prototype.send = function(data){
+ return typeof data == 'object' ? JSON.stringify(data): data;
+}
+
+function rand(ceiling){
+	return Math.floor(Math.random()*ceiling);
+}
+exports.Client = Client;
