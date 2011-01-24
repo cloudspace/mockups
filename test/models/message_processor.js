82a78b92f49572c24c612e4188a6cc69b6907c0c
Adrian Bravo <me@adrianbravo.net>
2011-01-24 11:37:52 -0500

    Added tests for #process/update_name

--- test/models/message_processor.js
+++ test/models/message_processor.js
@@ -1,17 +1,35 @@
 var
-	assert 	= require('assert'),
-	Project = require('../../lib/message_processor').MessageProcessor,
-	//User		= require('../../lib/user').User,
-  it      = require('../test_helper').it;
+	assert           = require('assert'),
+	MessageProcessor = require('../../lib/message_processor').MessageProcessor,
+	User             = require('../../lib/user').User,
+	it               = require('../test_helper').it;
 
-var
-	client	= { connection: { remoteAddress: '127.0.0.1' },sessionId:"1a" },
-	user		= new User(client);
+console.log("MessageProcessor");
 
-console.log("Message Processor");
+it("#process/update_name: changes a user's name", function() {
+	var client      = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
 
-it("#update_name:  sends out an announcement to the the current user and all other users of the name change", function() {
-	assert.equal( MessageProcessor.update_name(user.client, {new_name:"frank"}), '' );
+	MessageProcessor.process(client, { update_name: { new_name: 'Doug' } });
+	assert.equal(client.user.name, 'Doug');
 });
+
+it("#process/update_name: strips html", function() {
+	var client      = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
+
+	MessageProcessor.process(client, { update_name: { new_name: 'Doug<>' } });
+	assert.equal(client.user.name, 'Doug');
+});
+
+
+it("#process/update_name: strips whitespace", function() {
+	var client      = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
+
+	MessageProcessor.process(client, { update_name: { new_name: '   Doug   ' } });
+	assert.equal(client.user.name, 'Doug');
+});
+
 
 
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,31 +1,30 @@
 var sys = require('sys')
 var assert = require('assert')
 
-
 exports.it = function(description, test) {
-  try {
-    test();
+	try {
+		test();
 		sys.puts(" PASSED: " + description);
-  } catch (e) {
-    sys.puts(" FAILURE: " + description);
-    sys.puts("  " + e.stack);	
+	} catch (e) {
+		sys.puts(" FAILURE: " + description);
+		sys.puts("  " + e.stack);	
 		sys.puts("\n");
-  }
+	}
 }
 
-var Client = function (data) {
-		if(!data){
-			data = {remoteAddress: '127.0.0.1', sessionId: rand(10000) }
-		}
-		this.connection = {remoteAddress: data.remoteAddress};
-		this.sessionId = data.sessionId;
+var Client = function(data) {
+	if (!data) data = { remoteAddress: '127.0.0.1', sessionId: rand(10000) }
+	this.connection = { remoteAddress: data.remoteAddress };
+	this.sessionId = data.sessionId;
 };
 	
-Client.prototype.send = function(data){
- return typeof data == 'object' ? JSON.stringify(data): data;
+Client.prototype.send = function(data) {
+	return typeof data == 'object' ? JSON.stringify(data): data;
 }
 
-function rand(ceiling){
+function rand(ceiling) {
 	return Math.floor(Math.random()*ceiling);
 }
+
 exports.Client = Client;
+
