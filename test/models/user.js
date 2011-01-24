f434568fbf1b0fdf317bf8844740f233e0597ed3
Adrian Bravo <me@adrianbravo.net>
2011-01-24 14:54:33 -0500

    Added tests for User#assign_project.

--- test/models/user.js
+++ test/models/user.js
@@ -1,24 +1,51 @@
 var
 	assert 	= require('assert'),
-	Project = require('../../lib/project'),
 	User		= require('../../lib/user').User,
   it      = require('../test_helper').it;
-
-var
-	client	= { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a" },
-	user		= new User(client);
 
 console.log("User");
 
 it("instantiation fills default attributes", function() {
-	assert.equal('Anonymous', user.name);
-	assert.equal(client.connection.remoteAddress, user.ip);
-	assert.equal(0, user.project_id);
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
+
+	assert.equal('Anonymous', client.user.name);
+	assert.equal(client.connection.remoteAddress, client.user.ip);
+	assert.equal(client.user.project_id, undefined);
 });
 
 it("#handle: returns a user's name and IP address", function() {
-	assert.equal(user.handle(), 'Anonymous (127.0.0.1) ');
-	user.name = 'Frank';
-	assert.equal(user.handle(), 'Frank (127.0.0.1) ');
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
+
+	client.user.name = 'Frank';
+	assert.equal(client.user.handle(), 'Frank (127.0.0.1) ');
 });
+
+it("#assign_project: removes a user's current project and assigns one", function() {
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
+
+	client.user.assign_project(1);
+	assert.equal(client.user.project_id, 1);
+	client.user.assign_project(2);
+	assert.equal(client.user.project_id, 2);
+});
+
+it("#assign_project: returns true when successful", function() {
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
+
+	assert.equal(client.user.assign_project(3), true);
+	assert.equal(client.user.project_id, 3);
+});
+
+it("#assign_project: fails and returns false when not passed an id", function() {
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
+
+	assert.equal(client.user.assign_project(), false);
+	assert.equal(client.user.project_id, undefined);
+});
+
 
