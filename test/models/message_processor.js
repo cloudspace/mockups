6bf734711b075268a68e3985e4be36a7cf782d82
Adrian Bravo <me@adrianbravo.net>
2011-01-24 12:01:53 -0500

    Added tests for null update_name data. Added tests for #process re-routing data to processors or not throwing errors if non-existent processors are passed.

--- test/models/message_processor.js
+++ test/models/message_processor.js
@@ -6,8 +6,27 @@ var
 
 console.log("MessageProcessor");
 
+it("#process: routes methods with the appropriate data", function() {
+	// TODO DRY these up.
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
+
+	MessageProcessor.test = function(client, data){ client.zero = data; }; // Stub
+	MessageProcessor.process(client, { test: 0 });
+	assert.equal(client.zero, 0);
+});
+
+it("#process: catches unknown methods", function() {
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
+
+	assert.doesNotThrow(function(){
+		MessageProcessor.process(client, { test2: 0 })
+	});
+});
+
 it("#process/update_name: changes a user's name", function() {
-	var client      = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
 	client.user = new User(client);
 
 	MessageProcessor.process(client, { update_name: { new_name: 'Doug' } });
@@ -15,21 +34,27 @@ it("#process/update_name: changes a user's name", function() {
 });
 
 it("#process/update_name: strips html", function() {
-	var client      = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
 	client.user = new User(client);
 
 	MessageProcessor.process(client, { update_name: { new_name: 'Doug<>' } });
 	assert.equal(client.user.name, 'Doug');
 });
 
-
 it("#process/update_name: strips whitespace", function() {
-	var client      = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
 	client.user = new User(client);
 
 	MessageProcessor.process(client, { update_name: { new_name: '   Doug   ' } });
 	assert.equal(client.user.name, 'Doug');
 });
 
+it("#process/update_name: does not update when nil", function() {
+	var client  = { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a", send: function(){} };
+	client.user = new User(client);
+
+	MessageProcessor.process(client, { update_name: { new_name: null } });
+	assert.equal(client.user.name, 'Anonymous');
+});
 
 
