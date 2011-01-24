d2d917cf61af6f957ec0f928bd1b24045c0326f5
Adrian Bravo <me@adrianbravo.net>
2011-01-24 11:03:07 -0500

    Removed some duplicate code.

--- test/models/user.js
+++ test/models/user.js
@@ -5,7 +5,7 @@ var
   it      = require('../test_helper').it;
 
 var
-	client	= { connection: { remoteAddress: '127.0.0.1' },sessionId:"1a" },
+	client	= { connection: { remoteAddress: '127.0.0.1' }, sessionId: "1a" },
 	user		= new User(client);
 
 console.log("User");
@@ -16,26 +16,9 @@ it("instantiation fills default attributes", function() {
 	assert.equal(0, user.project_id);
 });
 
-var
-	assert 	= require('assert'),
-	Project = require('../../lib/project'),
-	User		= require('../../lib/user').User,
-  it      = require('../test_helper').it;
-
-var
-	client	= { connection: { remoteAddress: '127.0.0.1' },sessionId:"1a" },
-	user		= new User(client);
-
-console.log("User");
-
-it("instantiation fills default attributes", function() {
-	assert.equal('Anonymous', user.name);
-	assert.equal(client.connection.remoteAddress, user.ip);
-	assert.equal(0, user.project_id);
-});
-
-it("#handle: returns a user's name and Ip address", function() {
+it("#handle: returns a user's name and IP address", function() {
 	assert.equal(user.handle(), 'Anonymous (127.0.0.1) ');
 	user.name = 'Frank';
 	assert.equal(user.handle(), 'Frank (127.0.0.1) ');
 });
+
