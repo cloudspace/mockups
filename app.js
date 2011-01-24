ebdb82d53589a999bd675930e6919400ae35ac23
Adrian Bravo <me@adrianbravo.net>
2011-01-24 14:20:04 -0500

    Added publish, subscribe, unsubscribe(_all) to User library.

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var http = require('http')
   , fs = require('fs')
   , io = require('socket.io')
   , sys = require(process.binding('natives').util ? 'util' : 'sys')
-  , server;
+  , server
+  , clients = [];
 
 exports.sys			 = sys;
 exports.fs			 = fs;
-exports.channels = {};
+exports.clients  = clients;
 
 server = http.createServer(function(req, res){
 	var path = url.parse(req.url).pathname;
@@ -33,32 +34,25 @@ server.listen(8080);
 // Is this the proper notation?
 var User = require('./lib/user.js').User
   , MessageProcessor = require('./lib/message_processor').MessageProcessor
-  , Channel = require('./lib/channel.js').Channel
-  , io = io.listen(server)
-  , clients = {};
+  , io = io.listen(server);
 
 io.on('connection', function(client){
 
 	// These happen on the initial connection of a client.
-	clients[client.sessionId] = client; // Add client to clients list
-	client.user = new User(client);			// Set up client.user
-	client.send({ connected: '' });			// Respond with 'connected' message
-
-	// Let everyone know about the connection
-	// TODO restrict this to a project
-	client.broadcast({ announcement: client.user.ip + ' connected' });
+	clients.push(client);           // Add client to clients list
+	client.user = new User(client); // Set up client.user
+	client.send({ connected: '' }); // Respond with 'connected' message
 
 	client.on('message', function(message){
 		MessageProcessor.process(client, message);
 	});
 
 	client.on('disconnect', function(){
-		client.user.remove_channels();
+		client.user.unsubscribe_all();
 		// TODO restrict this to a project
-		client.broadcast({ announcement: client.user.ip + ' disconnected' });
+		//client.broadcast({ announcement: client.user.ip + ' disconnected' });
 	});
 });
-
 
 // We may want to add this elsewhere.
 Object.size = function(obj) {
@@ -68,4 +62,5 @@ Object.size = function(obj) {
 	}
 	return size;
 };
+
 
