c3466f50a78fb79f16f0302cf6998fdccc760c01
A Flores <ctrl4ltdeleteme@gmail.com>
2011-02-01 14:35:52 -0500

    [ #9452227] for Implement Front-end interaction Add/Update Page set up the implementation for css mockup updates

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -49,7 +49,7 @@ Project.prototype.sync_pages = function() {
 	var mockup_pages = $("#mockup_pages").html("");
 	for (var index in this.pages) {
 		var page = this.pages[index];
-		var span = $("<form><input type='text' value='" + page.name.toLowerCase() + "'/></form><span class='delete'> delete </span>").attr('data-id', index);
+		var span = $("<form class='name_update'><input type='text' value='" + page.name.toLowerCase() + "'/></form><span class='delete'> delete </span>").attr('data-id', index);
 		$('<li>/</li>').append(span).appendTo(mockup_pages);
 	}
 };
