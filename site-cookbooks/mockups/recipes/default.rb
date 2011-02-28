d15ec684f01f89deb0a56905f1b441deab1b3ee4
A Flores <ctrl4ltdeleteme@gmail.com>
2011-02-28 15:30:01 -0500

    [10408089] chef_config added a definition to install nodejs packages

--- site-cookbooks/mockups/recipes/default.rb
+++ site-cookbooks/mockups/recipes/default.rb
@@ -1,5 +1,7 @@
 
 include_recipe "nginx"
+include_recipe "nodejs::npm"
+
 
 template "#{node[:nginx][:dir]}/sites-available/mockups" do
   source "mockup-site.erb"
@@ -16,4 +18,4 @@ nginx_site "default" do
   enable false
 end
 
-
+npm_packages %w(mongodb@0.7.9 nodeunit@0.5.0 socket.io@0.6.8)
