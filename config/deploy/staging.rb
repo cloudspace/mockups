77bc20782014648fdd6b61400f7b7b13bbd95b30
A Flores <ctrl4ltdeleteme@gmail.com>
2011-03-03 12:30:44 -0500

    [10408089] added a file for launching new instances, thanks jlindsey!

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
-server 'ec2-50-17-84-219.compute-1.amazonaws.com', :app, :web, :primary => true
+server 'ec2-50-16-50-189.compute-1.amazonaws.com', :app, :web, :primary => true
 set :port, "22"
 set :admin_runner, 'root'
