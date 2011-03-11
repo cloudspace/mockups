0caaf9211097202507f434c39170dd677b06158f
A Flores <ctrl4ltdeleteme@gmail.com>
2011-03-11 13:43:46 -0500

    [10883051] General UI improvements - fix the expand collapse on toolbar

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -70,9 +70,11 @@ $(document).ready(function(){
 		  $("#tabs").slideToggle(300);
 			  $(this).toggleClass('collapsed');
 	});
-	$(".sectionbreak").click(function(){
+	
+	$(".section_break").click(function(){
 		$(this).toggleClass('collapsed').next().slideToggle(300);
 	});
+
 	$('form.canvas_object_update').live('submit',function(e){
 			var canvas_object = env.project.canvas_object($(this).attr('canvas_object_id'));
 			env.socket.send({
