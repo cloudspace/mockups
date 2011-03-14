1c997db8f90dc6a2c54689eba9caea4dfda4e725
A Flores <ctrl4ltdeleteme@gmail.com>
2011-03-14 12:31:03 -0400

    Forgot to uncomment more stuff.  :(

--- public/javascripts/message_processor.js
+++ public/javascripts/message_processor.js
@@ -144,7 +144,7 @@ MessageProcessor = {
 
 	connected: function() {
 		$('#connecting').dialog('destroy');
-		if (true){ !$.cookie('skipconnect')) {
+		if (!$.cookie('skipconnect')) {
 
   		$connected = $(Views.connected());
 			$connected
