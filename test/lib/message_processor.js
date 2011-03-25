a6c6aa67e95db70fa39451eb06fc1e2ee27f488c
Adrian Bravo <me@adrianbravo.net>
2011-03-25 12:03:18 -0400

    [#11543703] Added test for ignoring bad ids when deleting an array of canvas objects. Also added the ignore to canvas_object.delete--test passes.

--- test/lib/message_processor.js
+++ test/lib/message_processor.js
@@ -378,6 +378,27 @@ exports.process = testCase({
 		});
 	},
 
+	"canvas_object_delete: ignores invalid canvas_object ids": function(test) {
+		var that = this;
+		Project.create(function(project) {
+			// assign client to project
+			that.client.user.project_id = project._id;
+			that.client.user.subscribe(project._id);
+			Page.create(project, function(page) {
+				CanvasObject.create(page, { canvas_object: {} }, function(canvas_object) {
+					// delete canvas object
+					MessageProcessor.process(that.client, { canvas_object_delete: { page: { id: 1 }, canvas_objects: [{ id: 1 }, { id: 0 }] } });
+
+					setTimeout(function() {
+						test.equal(that.client.sent.error, undefined);
+						test.notEqual(that.client.sent.canvas_object_delete, undefined);
+						test.done();
+					}, 500);
+				});
+			});
+		});
+	},
+
 	"user_update: changes a user's name": function(test) {
 		MessageProcessor.process(this.client, { user_update: { name: 'Doug' } });
 		test.equals(this.client.user.name, 'Doug');
