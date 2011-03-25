1afb74ea3052d3c5075e4c1aeec560863ef0a4aa
A Flores <ctrl4ltdeleteme@gmail.com>
2011-03-25 11:44:19 -0400

    reorganized some code in the main file

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -46,9 +46,6 @@ $(document).ready(function(){
 
 	env = new Environment();
 	env.connect();
-});
-
-$(document).ready(function(){
 
 	$('#settings').hide();
 	$('#tabs h3').click(function(){
@@ -75,6 +72,74 @@ $(document).ready(function(){
 		$(this).hasClass('collapsed') ? $(this).html('+') : $(this).html('&ndash;');
 	});
 	
+	$('#help').live('click', function(e){
+		show_connected_screen(true);
+	});
+	
+	$('#floatingpanel').draggable({
+		handle: '#handler',
+		containment: 'html',
+	});
+
+	// Handle user changing their display name.
+	// TODO move to User
+	$('#name_change').submit(function(){
+		env.socket.send({ user_update: { name: $('#display_name').val() } });
+		$('#display_name').blur();
+		return false;
+	});
+
+	// Resets input field to current display_name.
+	// The only way this should get
+	// TODO move to User
+	$('#display_name').blur(function(){
+		reset_display_name();
+	});
+
+	
+	$('#mockup_pages .delete').live('click', function() {
+		if ($('#mockup_pages .delete').length == 1) {
+			alert("You can't delete the last page on a project.");
+		} else {
+			env.socket.send({ page_delete: { page_id: $(this).attr('page_id') } });
+		}
+	});
+
+	$('#add_page').live('click', function() {
+		env.socket.send({ page_create: true });
+	});
+
+	$('#mockup_pages a.selected').live('click', function(e) {
+		env.project.open_input_box($(e.target));
+		return false;
+	});
+
+
+	$('#mockup_pages .name_update input').live('blur', function(e) {
+		var $tgt = $(e.target), page_id = $tgt.parent().attr('page_id');
+		$tgt.addClass('h').siblings('a').removeClass('h');
+		$tgt.val(env.project.pages[page_id].name);
+	});
+
+
+	$('#mockup_pages li form.name_update').live('submit', function(){
+		var $page_name_input = $(this).find('input'), page_id = $(this).attr('page_id');
+		if ($page_name_input.val() == "") return false;
+		env.socket.send({
+			page_update: {
+				id: env.project.id,
+				hash: env.project.hash,
+				page: {
+					id: page_id,
+					name: $page_name_input.val()
+				}
+			}
+		});
+		$page_name_input.val(env.project.pages[page_id].name);
+		return false;
+	});
+
+
 	$('form.canvas_object_update').live('submit',function(e){
 			var canvas_object = env.project.canvas_object($(this).attr('canvas_object_id'));
 			env.socket.send({
@@ -95,6 +160,40 @@ $(document).ready(function(){
 			return false;	
 	});
 
+	$('.canvas_object_edit .delete').live('click', function(e) {
+		$('.canvas_object_edit').remove();
+		$(this).each(function() {
+			env.socket.send({
+				canvas_object_delete: {
+					canvas_object: { id: $(this).parents('form').attr('canvas_object_id') },
+					page:          { id: env.project.current_page }
+				}
+			});
+		});
+		return false;
+	});
+	
+	$('.canvas_object').live('dblclick', function(e) {
+		$('.canvas_object_edit').remove();
+		var $tgt = $(e.target), canvas_object_id = $(this).attr('canvas_object_id');
+
+		var $option_pane = $("<div></div>")
+			.addClass("canvas_object_edit").dialog({ 
+			closeOnEscape: true,
+			dialogClass:   'option_pane',
+			resizable:     false,
+			title:         'Edit Element'
+		}).html(Views.canvas_object_edit(canvas_object_id));
+
+		$(".canvas_object_update textarea").select();
+
+		$("#canvas").one('click', function(e) {
+			$('.option_pane').remove();
+			$('.canvas_object_edit').remove();
+			e.stopPropagation();
+		});
+	});
+	
 	$('#create_password form').live('submit',function() {
 		var password = $(this).find('#password').val();
 		if (password != $(this).find('#password_confirm').val()) {
@@ -130,40 +229,56 @@ $(document).ready(function(){
 		});
 	});	
 
-	$('.canvas_object_edit .delete').live('click', function(e) {
-		$('.canvas_object_edit').remove();
-		$(this).each(function() {
-			env.socket.send({
-				canvas_object_delete: {
-					canvas_object: { id: $(this).parents('form').attr('canvas_object_id') },
-					page:          { id: env.project.current_page }
-				}
-			});
-		});
-		return false;
-	});
-	
-	$('.canvas_object').live('dblclick', function(e) {
-		$('.canvas_object_edit').remove();
-		var $tgt = $(e.target), canvas_object_id = $(this).attr('canvas_object_id');
+		
 
-		var $option_pane = $("<div></div>")
-			.addClass("canvas_object_edit").dialog({ 
-			closeOnEscape: true,
-			dialogClass:   'option_pane',
-			resizable:     false,
-			title:         'Edit Element'
-		}).html(Views.canvas_object_edit(canvas_object_id));
+	$('#canvas').droppable({
+		drop: function(event, ui) {
+			$('input').blur();
 
-		$(".canvas_object_update textarea").select();
+			var $dragged_item    = $(ui.draggable), 
+					template_id      = $dragged_item.attr('template_id'),
+					canvas_object_id = $dragged_item.attr('canvas_object_id'), 
+					canvas_object    = env.project.canvas_object(canvas_object_id),
+					message          = {}; //,ui.position;
 
-		$("#canvas").one('click', function(e) {
-			$('.option_pane').remove();
-			$('.canvas_object_edit').remove();
-			e.stopPropagation();
-		});
+			// returns if the canvas_object's position is unchanged
+			if (canvas_object) {
+				if (ui.position.top == canvas_object.top && ui.position.left == canvas_object.left) return;
+			}
+			// if length > 0 then the dragged item is from the sidebar so it is a new canvas_object
+			var message_type = $dragged_item.parent('.elements').length > 0 ? 'canvas_object_create' : 'canvas_object_update';
+			message[message_type] = {
+				canvas_object: {
+					template_id: template_id,
+					top:         ui.position.top,
+					left:        ui.position.left,
+					id:          canvas_object_id,
+				},
+				page:        { id: env.project.current_page }
+			};
+			env.socket.send(message);
+		},
+		accept: '.elements li, .canvas_object'
 	});
 
+	$('#canvas').selectable({
+		cancel: '.clear',
+		filter: '.canvas_object', // We don't actually want children to be draggable or selectable.
+		delay: 10
+	});
+
+	$(window).click(function(e) {
+		// #canvas has a lot of click-capturing (draggable, droppable, selectable, resizable)
+		// so we force all input boxes to blur when it is clicked since those jquery plugins
+		// hijack the clicks and prevent the default behavior from occurring
+		$target = $(e.target);
+		if ($target.is('#canvas') || $target.parents('#canvas').length == 1) {
+			$('input').blur();
+			$('#canvas .ui-selected').removeClass('ui-selected');
+		}
+	});
+
+});
 	get_canvas_object_content = function(canvas_object_id){
 		var canvas_object = env.project.canvas_object(canvas_object_id);
 		return canvas_object.content ? canvas_object.content : templates[canvas_object.template_id].default_content;
@@ -222,131 +337,6 @@ $(document).ready(function(){
 		$('.instructional_marker').hide().remove(); 
 	}
 
-
-
-	$('.overlay').live('click',function(){
-		$(".overlay").remove();
-		$("#canvas .canvas_object, #floatingpanel, #growl").show();
-		$.cookie('skipconnect', true); 
-	});
-	
-	$('#help').live('click', function(e){
-		show_connected_screen(true);
-	});
-	
-	$('#floatingpanel').draggable({
-		handle: '#handler',
-		containment: 'html',
-	});
-
-	$('#canvas').droppable({
-		drop: function(event, ui) {
-			$('input').blur();
-
-			var $dragged_item    = $(ui.draggable), 
-					template_id      = $dragged_item.attr('template_id'),
-					canvas_object_id = $dragged_item.attr('canvas_object_id'), 
-					canvas_object    = env.project.canvas_object(canvas_object_id),
-					message          = {}; //,ui.position;
-
-			// returns if the canvas_object's position is unchanged
-			if (canvas_object) {
-				if (ui.position.top == canvas_object.top && ui.position.left == canvas_object.left) return;
-			}
-
-			// if length > 0 then the dragged item is from the sidebar so it is a new canvas_object
-			var message_type = $dragged_item.parent('.elements').length > 0 ? 'canvas_object_create' : 'canvas_object_update';
-			message[message_type] = {
-				canvas_object: {
-					template_id: template_id,
-					top:         ui.position.top,
-					left:        ui.position.left,
-					id:          canvas_object_id,
-				},
-				page:        { id: env.project.current_page }
-			};
-			env.socket.send(message);
-		},
-		accept: '.elements li, .canvas_object'
-	});
-
-	$('#canvas').selectable({
-		cancel: '.clear',
-		filter: '.canvas_object', // We don't actually want children to be draggable or selectable.
-		delay: 10
-	});
-
-	$(window).click(function(e) {
-		// #canvas has a lot of click-capturing (draggable, droppable, selectable, resizable)
-		// so we force all input boxes to blur when it is clicked since those jquery plugins
-		// hijack the clicks and prevent the default behavior from occurring
-		$target = $(e.target);
-		if ($target.is('#canvas') || $target.parents('#canvas').length == 1) {
-			$('input').blur();
-			$('#canvas .ui-selected').removeClass('ui-selected');
-		}
-	});
-
-	// Handle user changing their display name.
-	// TODO move to User
-	$('#name_change').submit(function(){
-		env.socket.send({ user_update: { name: $('#display_name').val() } });
-		$('#display_name').blur();
-		return false;
-	});
-
-	// Resets input field to current display_name.
-	// The only way this should get
-	// TODO move to User
-	$('#display_name').blur(function(){
-		reset_display_name();
-	});
-
-	
-	$('#mockup_pages .delete').live('click', function() {
-		if ($('#mockup_pages .delete').length == 1) {
-			alert("You can't delete the last page on a project.");
-		} else {
-			env.socket.send({ page_delete: { page_id: $(this).attr('page_id') } });
-		}
-	});
-
-	$('#add_page').live('click', function() {
-		env.socket.send({ page_create: true });
-	});
-
-	$('#mockup_pages a.selected').live('click', function(e) {
-		env.project.open_input_box($(e.target));
-		return false;
-	});
-
-
-	$('#mockup_pages .name_update input').live('blur', function(e) {
-		var $tgt = $(e.target), page_id = $tgt.parent().attr('page_id');
-		$tgt.addClass('h').siblings('a').removeClass('h');
-		$tgt.val(env.project.pages[page_id].name);
-	});
-
-
-
-	$('#mockup_pages li form.name_update').live('submit', function(){
-		var $page_name_input = $(this).find('input'), page_id = $(this).attr('page_id');
-		if ($page_name_input.val() == "") return false;
-		env.socket.send({
-			page_update: {
-				id: env.project.id,
-				hash: env.project.hash,
-				page: {
-					id: page_id,
-					name: $page_name_input.val()
-				}
-			}
-		});
-		$page_name_input.val(env.project.pages[page_id].name);
-		return false;
-	});
-});
-
 // TODO move to User
 function reset_display_name() {
 	$('#display_name').val(env.display_name);
@@ -368,6 +358,3 @@ function load_hash(hash) {
 		env.socket.send({ project_create: {} });
 	}
 }
-
-
-
